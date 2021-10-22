6775e42502494aa754b568e6d9c2718e692d3e9a
GazoNStyle GazoNStyle <basarievgan@gmail.com>
2021-10-22 08:18:22 +0300

    форма оранж и длинная

--- MyNotePad/Form1.Designer.cs
+++ MyNotePad/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace MyNotePad
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(70, 72);
+            this.button1.Location = new System.Drawing.Point(370, 95);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(103, 33);
             this.button1.TabIndex = 0;
@@ -46,8 +46,8 @@ namespace MyNotePad
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
-            this.ClientSize = new System.Drawing.Size(244, 167);
+            this.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.ClientSize = new System.Drawing.Size(842, 246);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
